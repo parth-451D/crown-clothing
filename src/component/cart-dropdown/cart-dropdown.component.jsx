e6d75461f38608f1a149b9ed2afa29d40279e4bd
parth-451D <parth.dalsaniya2001@gmail.com>
2021-07-24 20:25:41 +0530

    used selectors in every state

--- src/component/cart-dropdown/cart-dropdown.component.jsx
+++ src/component/cart-dropdown/cart-dropdown.component.jsx
@@ -3,6 +3,8 @@ import CustomButton from '../custom-button/custom-button.component';
 import './cart-dropdown.styles.scss';
 import { connect } from 'react-redux'; 
 import CartItem from '../cart-item/cart-item.component';
+import { selectCartItems } from '../../redux/cart/cart.selector';
+import { createStructuredSelector } from 'reselect';
 
 const CartDropdown = ({cartItems }) => (
  <div className="cart-dropdown">
@@ -16,7 +18,7 @@ const CartDropdown = ({cartItems }) => (
 );
 
 
-const mapStateToProps = ({cart: {cartItems}}) => ({
-   cartItems
+const mapStateToProps = createStructuredSelector ({
+   cartItems : selectCartItems
 })
 export default connect(mapStateToProps)(CartDropdown);
