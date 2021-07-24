56aca5cf54d90ed5ca19cad22bcfc91fa35fa8f0
parth-451D <parth.dalsaniya2001@gmail.com>
2021-07-24 20:47:49 +0530

    ERROR FOUND

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -14,6 +14,7 @@ const config = {
 };
 
 firebase.initializeApp(config);
+firebase.firestore().settings({ experimentalForceLongPolling: true });
 
 export const createUserProfileDocument = async ( userAuth, additionalData) => {
   if (!userAuth) return;
